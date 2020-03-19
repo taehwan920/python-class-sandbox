8af5833b6e01c1178f0249655c528ac6c1541be6
Yang Taehwan <taehwan920@gmail.com>
2020-03-19 17:09:30 +0900

    linkedList delete method is done

--- linked_list.py
+++ linked_list.py
@@ -6,10 +6,10 @@ class Node:
 
 class NodeManager:
     def __init__(self, data):
-        self.head = Node(data)
+        self.head = data
 
     def add(self, data):
-        if self.head is None:
+        if self.head == "":
             self.head = Node(data)
         else:
             node = self.head
@@ -22,3 +22,22 @@ class NodeManager:
         while node:
             print(node.data)
             node = node.next
+
+    def delete(self, data):
+        if self.head == "":
+            print('해당 노드는 없습니다')
+            return
+
+        if self.head.data == data:
+            temp = self.head
+            self.head = self.head.next
+            del temp
+        else:
+            node = self.head
+            while node.next:
+                if node.next.data == data:
+                    temp = node.next
+                    node.next = node.next.next
+                    del temp
+                else:
+                    node = node.next
