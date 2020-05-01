d0757453d19d7b67ab1729b0a1e867d07b1201ce
Yang Taehwan <taehwan920@gmail.com>
2020-05-02 00:24:53 +0900

    BST insert method

---
+++ BST.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class NodeMgnt:
+    def __init__(self, head):
+        self.head = head
+
+    def insert(self, value):
+        self.current_node = self.head
+        while True:
+            if value < self.current_node:
+                if self.current_node.left != None:
+                    self.current_node.left = value
+                else:
+                    self.current_node.left = Node(value)
+                    break
+            else:
+                if self.current_node.right != None:
+                    self.current_node.right = value
+                else:
+                    self.current_node.right = Node(value)
+                    break
